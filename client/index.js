cbe9e22ef2057dc2061a2b5373f62ed3a9c7711f
Tracy Chang <Tracy@Andrews-MacBook-Air.local>
2022-10-31 19:06:26 -0700

    client

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 
 
 import React from 'react';
-// import ReactDOM from 'react-dom';
+import { ReactDOM } from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App.jsx';
